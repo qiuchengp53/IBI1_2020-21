525bb60760aae8e051106821ba4d602d6636add1
qiuchengp53 <chengq.20@intl.zju.edu.cn>
2021-03-10 09:43:28 +0800

    boolean

--- Practical5/variables.py
+++ Practical5/variables.py
@@ -9,4 +9,12 @@ e = abs(a-b)
 if d - e > 0:
     print("d is greater")
 else:
-    print("e is greater")
+    print("e is greater") 
+X = False 
+Y = True 
+Z = (X and not Y) or (Y and not X) 
+print(Z) 
+W =(X!=Y) 
+if W == Z:
+    print("W and Z are always the same")
+ 
