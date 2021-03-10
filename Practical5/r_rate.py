6ca9a797b0ecd3954fe150b791ad5aec051e156f
qiuchengp53 <chengq.20@intl.zju.edu.cn>
2021-03-10 11:09:31 +0800

    r_rate

---
+++ Practical5/r_rate.py
@@ -0,0 +1,13 @@
+#change the r_rate easiler and don't need to change the command 
+print("Input the r_rate number below")
+#define the variable
+n = 84
+#input r
+r = float (input("r_rate: "))
+#loop
+for i in range (1,6):
+    n = n*r
+#remove the decimal
+a = str(int(n))
+#output the result
+print(a+" is the number of individuals that infected after 5 generation when r = "+str(r))
