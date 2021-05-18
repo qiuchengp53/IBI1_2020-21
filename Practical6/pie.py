00fdeb0b91b5e07fc730d3bf95fbccf58386ff45
qiuchengp53 <79952195+qiuchengp53@users.noreply.github.com>
2021-05-18 23:29:32 +0800

    Update pie.py

--- Practical6/pie.py
+++ Practical6/pie.py
@@ -5,9 +5,11 @@ import numpy as np
 # pie chart
 labels = 'USA','India','Brazil','Russia','UK'
 sizes = [29862124, 11285561, 11205972, 4360823, 4234924]
+# input the size and data
 case = dict(zip(labels,sizes))
 print(case)
 plt.pie(sizes, labels=labels, autopct='%1.1f%%',shadow=False, startangle=90)
+# input some parameter
 plt.axis('equal')
 plt.title("comparing coronavirus infection rates across countries")
 #show the pie chart
